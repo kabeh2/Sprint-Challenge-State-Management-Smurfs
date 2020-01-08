0d7d5d18730df7ab3342f9da277839a29929a669
kabeh2 <mattcnk@hotmail.com>
2020-01-07 19:55:13 -0500

    Updates

---
+++ precache-manifest.978f7f57d44d8facc4b8bdf58eb31442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6b3c19bc07e906b2d6265a6ac82387",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/index.html"
+  },
+  {
+    "revision": "a4c72c07126e982f8f53",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "d7a9e9a75b0ee072e96b",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/css/main.5016c4f0.chunk.css"
+  },
+  {
+    "revision": "a4c72c07126e982f8f53",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/2.a100cd41.chunk.js"
+  },
+  {
+    "revision": "77f907c2c53f663b4bb8c2c0a981dafb",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/2.a100cd41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7a9e9a75b0ee072e96b",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/main.282965b5.chunk.js"
+  },
+  {
+    "revision": "4f3ccfe01dba9d9824f6",
+    "url": "/Sprint-Challenge-State-Management-Smurfs/static/js/runtime-main.a497d0ff.js"
+  }
+]);
